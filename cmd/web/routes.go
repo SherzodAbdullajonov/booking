27344fb2872bc8fec7afcfc3c72ca9cff985c274
SherzodAbdullajonov <sabdullajonov77@gmail.com>
2022-04-21 13:14:24 +0500

    go templates created

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,10 @@ func Routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/general-quarter", handlers.Repo.Generals)
+	mux.Get("/major-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/contact", handlers.Repo.Contact)
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
